5aea4b0695f4cf8bf5024d527987f34c659a8182
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2022-11-13 14:16:00 +0530

    changes done

--- app/src/main/java/com/androidready/demo/MainActivity.kt
+++ app/src/main/java/com/androidready/demo/MainActivity.kt
@@ -7,5 +7,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("Activity : onCreate")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("Activity : onStart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("Activity : onResume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("Activity : onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("Activity : onStop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("Activity : onDestroy")
+
     }
 }
