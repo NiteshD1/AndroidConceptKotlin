d6604bdc76b75c263fec0cce8f72e3c372610191
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2022-11-15 17:07:41 +0530

    changes done

--- app/src/main/java/com/androidready/demo/MainActivity.kt
+++ app/src/main/java/com/androidready/demo/MainActivity.kt
@@ -3,18 +3,25 @@ package com.androidready.demo
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroupOverlay
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE
 import androidx.fragment.app.FragmentOnAttachListener
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.Navigation
 import com.androidready.demo.R.*
 import com.androidready.demo.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity(),View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener,
+    NavigationView.OnNavigationItemSelectedListener {
 
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     private lateinit var binding: ActivityMainBinding
     val fragmentManager = supportFragmentManager
     private lateinit var fragmentTransaction: FragmentTransaction
@@ -27,42 +34,30 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         setContentView(view)
         println("Activity : onCreate")
 
-        fragmentManager.addFragmentOnAttachListener(
-            FragmentOnAttachListener{ fragmentManager, fragment ->
-                if(fragment == fragmentManager.findFragmentByTag("home")){
-                     println("Home Fragment is attached")
-                }
-//                if(fragment == fragmentManager.findFragmentById(R.layout.fragment_home)){
-//                    println("Home Fragment is attached")
-//                }
-        })
+
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, binding.drawerLayout, string.nav_open, string.nav_close)
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        // to make the Navigation drawer icon always appear on the action bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
 
     }
 
-
-    private fun addFragment(fragment : Fragment) {
-
-
-        fragmentTransaction = fragmentManager.beginTransaction()
-       // fragmentTransaction.add(id.frameLayout,fragment)
-
-        //fragmentTransaction.replace(R.id.frameLayout,fragment)
-        //fragmentTransaction.remove(R.id.frameLayout,fragment)
-
-        if(fragment is HomeFragment){
-            fragmentManager.popBackStack("home_fragment",POP_BACK_STACK_INCLUSIVE)
-            fragmentTransaction.add(id.frameLayout,fragment,"home")
-            fragmentTransaction.addToBackStack("home_fragment")
-        }else{
-            fragmentTransaction.add(id.frameLayout,fragment)
-            fragmentTransaction.addToBackStack(null)
-        }
-
-        fragmentTransaction.commit()
-
-        println("Activity : Fragment Added")
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
     }
+
+
 
     override fun onStart() {
         super.onStart()
@@ -102,11 +97,23 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         super.onBackPressed()
     }
 
-    override fun onClick(view : View?) {
+    override fun onClick(view: View?) {
 
 
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_setting -> {
+                println("setting selected")
+                Navigation.findNavController(this, R.id.fragmentContainerView)
+                    .navigate(R.id.action_global_settingFragment)
+            }
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
 
 
 }
