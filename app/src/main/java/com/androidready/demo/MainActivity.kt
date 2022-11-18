42ce81111430a029a7c3c2de5e4688df5a65608f
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2022-11-18 21:58:07 +0530

    App permission Changes done

--- app/src/main/java/com/androidready/demo/MainActivity.kt
+++ app/src/main/java/com/androidready/demo/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.androidready.demo
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.androidready.demo.databinding.ActivityMainBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class MainActivity : AppCompatActivity(), View.OnClickListener{
 
@@ -19,17 +30,77 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
         setContentView(view)
         println("Activity : onCreate")
 
-        setupCustomDialog()
-
+        binding.buttonRequestPermission.setOnClickListener(View.OnClickListener {
+            requestPermissions()
+        })
 
     }
 
-    private fun setupCustomDialog() {
-        binding.buttonCustomDialog.setOnClickListener {
-            CustomDialog.newInstance(getString(R.string.dialog_heading), getString(R.string.dialog_description))
-                .show(supportFragmentManager, CustomDialog.TAG)
+    private fun requestPermissions() {
+        // below line is use to request permission in the current activity.
+        // this method is use to handle error in runtime permissions
+        Dexter.withActivity(this)
+            // below line is use to request the number of permissions which are required in our app.
+            .withPermissions(
+                Manifest.permission.CAMERA,
+                // below is the list of permissions
+                Manifest.permission.READ_CONTACTS)
+            // after adding permissions we are calling an with listener method.
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(multiplePermissionsReport: MultiplePermissionsReport) {
+                    // this method is called when all permissions are granted
+                    if (multiplePermissionsReport.areAllPermissionsGranted()) {
+                        // do you work now
+                        Toast.makeText(this@MainActivity, "All the permissions are granted..", Toast.LENGTH_SHORT).show()
+                    }
+                    // check for permanent denial of any permission
+                    if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied) {
+                        // permission is denied permanently, we will show user a dialog message.
+                        showSettingsDialog()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(list: List<PermissionRequest>, permissionToken: PermissionToken) {
+                    // this method is called when user grants some permission and denies some of them.
+                    permissionToken.continuePermissionRequest()
+                }
+            }).withErrorListener {
+                // we are displaying a toast message for error message.
+                Toast.makeText(applicationContext, "Error occurred! ", Toast.LENGTH_SHORT).show()
+            }
+            // below line is use to run the permissions on same thread and to check the permissions
+            .onSameThread().check()
+    }
+
+    // below is the shoe setting dialog method
+    // which is use to display a dialogue message.
+    private fun showSettingsDialog() {
+        // we are displaying an alert dialog for permissions
+        val builder = AlertDialog.Builder(this@MainActivity)
+
+        // below line is the title for our alert dialog.
+        builder.setTitle("Need Permissions")
+
+        // below line is our message for our dialog
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.")
+        builder.setPositiveButton("GOTO SETTINGS") { dialog, which ->
+            // this method is called on click on positive button and on clicking shit button
+            // we are redirecting our user from our app to the settings page of our app.
+            dialog.cancel()
+            // below is the intent from which we are redirecting our user.
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val uri = Uri.fromParts("package", packageName, null)
+            intent.data = uri
+            startActivityForResult(intent, 101)
         }
+        builder.setNegativeButton("Cancel") { dialog, which ->
+            // this method is called when user click on negative button.
+            dialog.cancel()
+        }
+        // below line is used to display our dialog
+        builder.show()
     }
+
 
 
     override fun onStart() {
