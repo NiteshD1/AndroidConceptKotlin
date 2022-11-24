6f04d3e9a35c3513354bd1327e1317a40f98060b
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2022-11-24 16:49:41 +0530

    File storage changes done

--- app/src/main/java/com/androidready/demo/MainActivity.kt
+++ app/src/main/java/com/androidready/demo/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.androidready.demo
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.*
 import com.androidready.demo.api.RetrofitInstance
 import com.androidready.demo.databinding.ActivityMainBinding
-import com.androidready.demo.worker.BackgroundTaskWorker
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.coroutines.*
-import java.util.concurrent.TimeUnit
+import java.io.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener{
 
+    private lateinit var myExternalFile: File
     private lateinit var binding: ActivityMainBinding
-
+    val filename = "myfile"
+    val externalFileName = "AndroidReadyDemoFile"
+    private val externalStorageFilePath = "AndroidReadyFolder"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,42 +39,182 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
         setContentView(view)
         println("Activity : onCreate")
 
-        binding.buttonStartWorkManager.setOnClickListener(View.OnClickListener {
-            startWorkManager()
+        //createFileInternalStorage()
+
+        binding.buttonWriteInternal.setOnClickListener(View.OnClickListener {
+            writeInPrivateFile()
         })
 
-        binding.buttonStopWorkManager.setOnClickListener(View.OnClickListener {
-            stopWorkManager()
+        binding.buttonReadInternal.setOnClickListener(View.OnClickListener {
+            readFromPrivateFile()
+        })
+
+        binding.buttonRequestPermission.setOnClickListener(View.OnClickListener {
+            requestPermissions()
+        })
+
+        binding.buttonWriteExternal.setOnClickListener(View.OnClickListener {
+            writeInExternal()
+        })
+
+        binding.buttonReadExternal.setOnClickListener(View.OnClickListener {
+            readFromExternalFile()
         })
         binding.buttonFetchData.setOnClickListener(View.OnClickListener {
             retrofitDemo()
         })
     }
 
-    private fun stopWorkManager() {
-        WorkManager.getInstance(MainApplication.appContext).cancelAllWorkByTag("worker")
+    private fun readFromExternalFile() {
+        myExternalFile = File(getExternalFilesDir(externalStorageFilePath), externalFileName)
+
+        val stringBuffer = StringBuffer()
+        try {
+            //Attaching BufferedReader to the FileInputStream by the help of InputStreamReader
+            val inputReader = InputStreamReader(FileInputStream(myExternalFile))
+            inputReader.forEachLine { stringBuffer.append(it) }
+
+            stringBuffer.let {
+                binding.textView.text = it.toString()
+            }
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 
-    private fun startWorkManager() {
+    private fun writeInExternal() {
+        val outputStream: FileOutputStream
+        var fileContents = "Dummy Content"
 
-//        val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
-//        val oneTimeRequest = OneTimeWorkRequestBuilder<BackgroundTaskWorker>()
-//            .setConstraints(constraints)
-//            .addTag("worker")
-//            .setInitialDelay(10,TimeUnit.SECONDS)
-//            .build()
-//
-//        WorkManager.getInstance(MainApplication.appContext).enqueue(oneTimeRequest)
+        binding.editText.let {
+            fileContents = it.text.toString()
+        }
 
-        val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
-        val oneTimeRequest = PeriodicWorkRequestBuilder<BackgroundTaskWorker>(10,TimeUnit.SECONDS)
-            .setConstraints(constraints)
-            .addTag("worker")
-            .setInitialDelay(10,TimeUnit.SECONDS)
-            .build()
-
-        WorkManager.getInstance(MainApplication.appContext).enqueue(oneTimeRequest)
+        myExternalFile = File(getExternalFilesDir(externalStorageFilePath), externalFileName)
+        try {
+            outputStream = FileOutputStream(myExternalFile)
+            outputStream.write(fileContents.toByteArray())
+            outputStream.close()
+            Utils.showToast("Write Operation Done!")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
+
+    private val isExternalStorageReadOnly: Boolean get() {
+        val extStorageState = Environment.getExternalStorageState()
+        return Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)
+    }
+    private val isExternalStorageAvailable: Boolean get() {
+        val extStorageState = Environment.getExternalStorageState()
+        return Environment.MEDIA_MOUNTED.equals(extStorageState)
+    }
+
+    private fun requestPermissions() {
+        // below line is use to request permission in the current activity.
+        // this method is use to handle error in runtime permissions
+        Dexter.withActivity(this)
+            // below line is use to request the number of permissions which are required in our app.
+            .withPermissions(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            // after adding permissions we are calling an with listener method.
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(multiplePermissionsReport: MultiplePermissionsReport) {
+                    // this method is called when all permissions are granted
+                    if (multiplePermissionsReport.areAllPermissionsGranted()) {
+                        // do you work now
+                        Toast.makeText(this@MainActivity, "All the permissions are granted..", Toast.LENGTH_SHORT).show()
+                    }
+                    // check for permanent denial of any permission
+                    if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied) {
+                        // permission is denied permanently, we will show user a dialog message.
+                        showSettingsDialog()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(list: List<PermissionRequest>, permissionToken: PermissionToken) {
+                    // this method is called when user grants some permission and denies some of them.
+                    permissionToken.continuePermissionRequest()
+                }
+            }).withErrorListener {
+                // we are displaying a toast message for error message.
+                Toast.makeText(applicationContext, "Error occurred! ", Toast.LENGTH_SHORT).show()
+            }
+            // below line is use to run the permissions on same thread and to check the permissions
+            .onSameThread().check()
+    }
+
+
+    // below is the shoe setting dialog method
+    // which is use to display a dialogue message.
+    private fun showSettingsDialog() {
+        // we are displaying an alert dialog for permissions
+        val builder = AlertDialog.Builder(this@MainActivity)
+
+        // below line is the title for our alert dialog.
+        builder.setTitle("Need Permissions")
+
+        // below line is our message for our dialog
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.")
+        builder.setPositiveButton("GOTO SETTINGS") { dialog, which ->
+            // this method is called on click on positive button and on clicking shit button
+            // we are redirecting our user from our app to the settings page of our app.
+            dialog.cancel()
+            // below is the intent from which we are redirecting our user.
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val uri = Uri.fromParts("package", packageName, null)
+            intent.data = uri
+            startActivityForResult(intent, 101)
+        }
+        builder.setNegativeButton("Cancel") { dialog, which ->
+            // this method is called when user click on negative button.
+            dialog.cancel()
+        }
+        // below line is used to display our dialog
+        builder.show()
+    }
+    private fun readFromPrivateFile() {
+        val stringBuffer = StringBuffer()
+        try {
+            //Attaching BufferedReader to the FileInputStream by the help of InputStreamReader
+            val inputReader = BufferedReader(InputStreamReader(openFileInput(filename)))
+            inputReader.forEachLine { stringBuffer.append(it) }
+
+            stringBuffer.let {
+                binding.textView.text = it.toString()
+            }
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+
+    private fun writeInPrivateFile() {
+
+        val outputStream: FileOutputStream
+        var fileContents = "Dummy Content"
+
+        binding.editText.let {
+            fileContents = it.text.toString()
+        }
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE)
+            outputStream.write(fileContents.toByteArray())
+            outputStream.close()
+            Utils.showToast("Write Operation Done!")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun createFileInternalStorage() {
+        val file = File(this.filesDir, "demoFile")
+        println("File Path is : ${file.absolutePath}")
+    }
+
 
     private fun retrofitDemo() {
         GlobalScope.launch(Dispatchers.IO) {
