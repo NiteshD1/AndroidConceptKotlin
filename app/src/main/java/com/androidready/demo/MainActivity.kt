eeda80e222000b96474cb2b4a3cca22dc228394c
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2022-12-19 15:18:58 +0530

    changes done

--- app/src/main/java/com/androidready/demo/MainActivity.kt
+++ app/src/main/java/com/androidready/demo/MainActivity.kt
@@ -4,35 +4,73 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.androidready.demo.databinding.ActivityMainBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity(), View.OnClickListener{
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
+
+
         setContentView(view)
         println("Activity : onCreate")
 
-        setupBottomSheet()
 
+        setupBottomSheetModel()
+
+        setupPersistentBottomSheet()
 
     }
 
+    private fun setupPersistentBottomSheet() {
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheetLayout.bottomSheet)
 
-    private fun setupBottomSheet() {
-        binding.buttonBottomSheet.setOnClickListener {
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                // handle onSlide
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> Toast.makeText(this@MainActivity, "STATE_COLLAPSED", Toast.LENGTH_SHORT).show()
+                    BottomSheetBehavior.STATE_EXPANDED -> Toast.makeText(this@MainActivity, "STATE_EXPANDED", Toast.LENGTH_SHORT).show()
+                    BottomSheetBehavior.STATE_DRAGGING -> Toast.makeText(this@MainActivity, "STATE_DRAGGING", Toast.LENGTH_SHORT).show()
+                    BottomSheetBehavior.STATE_SETTLING -> Toast.makeText(this@MainActivity, "STATE_SETTLING", Toast.LENGTH_SHORT).show()
+                    BottomSheetBehavior.STATE_HIDDEN -> Toast.makeText(this@MainActivity, "STATE_HIDDEN", Toast.LENGTH_SHORT).show()
+                    else -> Toast.makeText(this@MainActivity, "OTHER_STATE", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+        binding.buttonBottomSheetPersistent.setOnClickListener {
+            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED)
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            else
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+    }
+
+
+    private fun setupBottomSheetModel() {
+        binding.buttonBottomSheetModel.setOnClickListener {
 
             // on below line we are creating a new bottom sheet dialog.
             val dialog = BottomSheetDialog(this)
 
             // on below line we are inflating a layout file which we have created.
-            val view = layoutInflater.inflate(R.layout.bottom_sheet, null)
+            val view = layoutInflater.inflate(R.layout.bottom_sheet_model, null)
 
             // on below line we are creating a variable for our button
             // which we are using to dismiss our dialog.
